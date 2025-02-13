b27a4248125d728f654eaf3979689c7714aa38c2
mudasirshah434 <mudasirshah9777@gmail.com>
2025-02-14 01:33:23 +0500

    some debugging

--- Backend/src/controllers/blog.controller.js
+++ Backend/src/controllers/blog.controller.js
@@ -115,22 +115,25 @@ const updateBlog = asyncHandler(async (req, res) => {
 
 // Delete a blog
 const deleteBlog = asyncHandler(async (req, res) => {
-    console.log("Delete Blog Controller: Deleting blog with ID", req.params.id);
+    const { id } = req.params;
+    console.log(`Delete Blog Controller: Deleting blog with ID ${id}`);
 
-    const blog = await Blog.findById(req.params.id);
+    // Find the blog post
+    const blog = await Blog.findById(id);
+
     if (!blog) {
-        console.error("Error: Blog not found");
         throw new ApiError(404, "Blog not found");
     }
 
+    // Ensure only the author can delete
     if (blog.author.toString() !== req.user._id.toString()) {
-        console.error("Error: Unauthorized to delete this blog");
         throw new ApiError(403, "You are not authorized to delete this blog");
     }
 
     console.log("Blog found, deleting...");
-    await blog.remove();
-    console.log("Blog deleted");
+
+    // Delete the blog properly
+    await Blog.deleteOne({ _id: id });
 
     res.status(200).json({
         success: true,
